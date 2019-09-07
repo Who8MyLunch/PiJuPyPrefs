97fa126c31744809245188ab9093b3099727107e
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2019-09-07 11:02:42 -0700

    update mpl settings

--- pijupy/my_matplotlib.py
+++ pijupy/my_matplotlib.py
@@ -6,4 +6,10 @@ import matplotlib.pyplot as plt
 def apply():
     """Apply all MPL settings
     """
+    # print(plt.style.available)
+    # plt.style.use('seaborn-paper')
     plt.style.use('seaborn-notebook')
+
+    plt.rcParams['figure.max_open_warning'] = 1000
+
+    plt.rcParams['figure.figsize'] = (12, 6)
