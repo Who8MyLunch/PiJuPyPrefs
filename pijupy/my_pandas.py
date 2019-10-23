6758e2cf3017c9824beca18a3602a467ebe66d9c
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2019-10-23 07:29:51 -0700

    update pandas settings

--- pijupy/my_pandas.py
+++ pijupy/my_pandas.py
@@ -1,9 +1,15 @@
 
 import pandas as pd
 
-
+"""
+https://pandas.pydata.org/pandas-docs/stable/user_guide/options.html
+"""
 
 def apply():
     """Apply all pandas settings functions
     """
-    pass
+    # pd.options.display.max_rows = 999
+    pd.options.display.latex.repr = True
+    pd.options.display.html.table_schema = True
+
+    # pd.options.display.latex.longtable = False  # default False
