456405b1496ecda1f14f7f1e6947618a6ff07b63
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2019-12-16 09:48:05 -0800

    update

--- pijupy/my_pandas.py
+++ pijupy/my_pandas.py
@@ -7,8 +7,11 @@ https://pandas.pydata.org/pandas-docs/stable/user_guide/options.html
 
 def apply():
     """Apply all pandas settings functions
+
+    https://pandas.pydata.org/pandas-docs/stable/user_guide/style.html
     """
     # pd.options.display.max_rows = 999
+    pd.options.display.max_columns = None
     pd.options.display.latex.repr = True
     pd.options.display.html.table_schema = True
 
